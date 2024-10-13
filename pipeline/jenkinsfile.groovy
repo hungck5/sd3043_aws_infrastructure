6d85ff65cbbc4bc1a73961ebb4817a4a3e89a308
hung.nq <hung.nq@vlinkgroup.net>
2024-10-13 08:48:31 +0700

    update jenkinsfile

--- pipeline/jenkinsfile.groovy
+++ pipeline/jenkinsfile.groovy
@@ -1,20 +1,13 @@
 pipeline {
     agent {
         docker {
-            image 'ubuntu:20.04'  // Sử dụng một image Ubuntu cơ bản
-            args '-v /var/run/docker.sock:/var/run/docker.sock'
+            dockerfile 
+            { 
+                dir 'src/frontend' 
+            }
         }
     } 
     stages {
-         stage('Install Docker CLI') {
-            steps {
-                sh '''
-                   apt-get update
-                   apt-get install -y docker.io
-                   docker --version
-                '''
-            }
-        }
         stage('step 1') {
             steps {
                 echo 'Hello world!' 
