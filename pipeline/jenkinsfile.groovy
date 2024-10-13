738745abc8d994c4587b9e5204e6f566360feb28
hung.nq <hung.nq@vlinkgroup.net>
2024-10-13 11:03:15 +0700

    disable tsl

--- pipeline/jenkinsfile.groovy
+++ pipeline/jenkinsfile.groovy
@@ -1,6 +1,15 @@
 pipeline {
     agent any
     stages {
+        stage('Disable TLS') {
+            steps {
+                sh '''
+                    unset DOCKER_TLS_VERIFY
+                    unset DOCKER_CERT_PATH
+                    unset DOCKER_HOST
+                '''
+            }
+        }
         stage('step 1') {
             steps {
                 echo 'Hello world!' 
