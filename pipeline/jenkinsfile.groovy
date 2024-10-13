3b75768df4a252247812eafe8638555bc9b55991
hung.nq <hung.nq@vlinkgroup.net>
2024-10-13 08:24:01 +0700

    fix docker install jenkin

--- pipeline/jenkinsfile.groovy
+++ pipeline/jenkinsfile.groovy
@@ -1,11 +1,20 @@
 pipeline {
     agent {
         docker {
-            image 'docker:20.10.7'
+            image 'ubuntu:20.04'  // Sử dụng một image Ubuntu cơ bản
             args '-v /var/run/docker.sock:/var/run/docker.sock'
         }
     } 
     stages {
+         stage('Install Docker CLI') {
+            steps {
+                sh '''
+                   apt-get update
+                   apt-get install -y docker.io
+                   docker --version
+                '''
+            }
+        }
         stage('step 1') {
             steps {
                 echo 'Hello world!' 
