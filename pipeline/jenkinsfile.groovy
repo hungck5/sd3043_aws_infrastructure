b80d9c654a99597f703882869abd64d7e0fc9bd7
hung.nq <hung.nq@vlinkgroup.net>
2024-10-13 12:16:54 +0700

    test 2

--- pipeline/jenkinsfile.groovy
+++ pipeline/jenkinsfile.groovy
@@ -1,16 +1,7 @@
 pipeline {
     agent any
     stages {
-        stage('Disable TLS') {
-            steps {
-                sh '''
-                    unset DOCKER_TLS_VERIFY
-                    unset DOCKER_CERT_PATH
-                    unset DOCKER_HOST
-                    docker --version
-                '''
-            }
-        }
+        
         stage('step 1') {
             steps {
                 echo 'Hello world!' 
@@ -18,7 +9,12 @@ pipeline {
         }
         stage('step 2') {
             steps {
-                sh 'docker --version' 
+                sh '''
+                    unset DOCKER_TLS_VERIFY
+                    unset DOCKER_CERT_PATH
+                    unset DOCKER_HOST
+                    docker --version
+                ''' 
             }
         }
     }
