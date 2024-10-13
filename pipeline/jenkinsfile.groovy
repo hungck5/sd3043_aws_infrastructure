7ab83ec153224b6c6c96d4994039d5a414404213
hung.nq <hung.nq@vlinkgroup.net>
2024-10-13 07:46:00 +0700

    Update agent docker

--- pipeline/jenkinsfile.groovy
+++ pipeline/jenkinsfile.groovy
@@ -1,5 +1,10 @@
 pipeline {
-    agent any 
+    agent {
+        docker {
+            image 'docker:25.0.3'
+            args '-v /var/run/docker.sock:/var/run/docker.sock'
+        }
+    } 
     stages {
         stage('step 1') {
             steps {
